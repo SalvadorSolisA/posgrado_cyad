764b39ef6603c2f2e90b441a7b34594c22e6ec63
Salvador Solis <al2123032568@azc.uam.mx>
2022-06-18 22:29:43 -0500

    update

---
+++ src/test/java/com/example/PosgradoCyad/PosgradoCyadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PosgradoCyad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosgradoCyadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
